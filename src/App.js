0c9ad6ca9e1a9524e45f0d06e803f9576ec099b5
adamwisnup <wisnuadam21@gmail.com>
2022-09-05 00:09:18 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,75 +1,104 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import ParticlesBg from "particles-bg";
-import './App.css';
+import "./App.css";
 import { BsInstagram } from "react-icons/bs";
-import { FaTiktok } from "react-icons/fa";
-import { FiYoutube } from "react-icons/fi";
+import { GiPapers } from "react-icons/gi";
+import { MdOutlineContactPhone } from "react-icons/md";
 import TextLoop from "react-text-loop";
-import Typist from 'react-typist';
+import Typist from "react-typist";
 
 function App() {
-	const config_cursor = {
-		show: true,
-		blink: true,
-		element: '🔨',
-		hideWhenDone: true,
-	}
+  const config_cursor = {
+    show: true,
+    blink: true,
+    element: "🔨",
+    hideWhenDone: true,
+  };
 
-	const [darkmode, setdarkmode] = useState(false);
+  const [darkmode, setdarkmode] = useState(false);
 
-	return (
-		<div>
-			<div className={`card ${darkmode ? 'dark' : ''}`}>
-				<div className="toggle-btn" onClick={() => setdarkmode(!darkmode)}></div>
-				<div className="dark-mode"></div>
-				<div className="card_body">
-					<div className="profile text-center ">
-						<img src="/logo192.png" className="avatar" alt="avatar" />
-						<div className="bg_content rd_12 p_8">
-							<h1>@TryCatch</h1>
-							<p className='mt-16'>Seputar Programming</p>
-							<Typist cursor={config_cursor}>
-								Terima Jasa Pembuatan Website, Android Apps & IOS Apps
-							</Typist>
-						</div>
+  return (
+    <div>
+      <div className={`card ${darkmode ? "dark" : ""}`}>
+        <div
+          className="toggle-btn"
+          onClick={() => setdarkmode(!darkmode)}
+        ></div>
+        <div className="dark-mode"></div>
+        <div className="card_body">
+          <div className="profile text-center ">
+            <img src="/Artboard 9.png" className="avatar" alt="avatar" />
+            <div className="bg_content rd_12 p_8">
+              <h1>RIOST</h1>
+              <p className="mt-16">
+                <strong>Design Graphic</strong>
+              </p>
+              <Typist cursor={config_cursor}>
+                Desain Komunikasi Visual | Freelance Logo | Poster | Icon |
+                Vector
+              </Typist>
+            </div>
 
-						<div className="bg_content rd_12 p_8 mt-16">
-							Familiar with{' '}
-							<TextLoop interval={800}>
-								<span className='higthlight'>Laravel</span>
-								<span className='higthlight'>Lumen</span>
-								<span className='higthlight'>React JS</span>
-								<span className='higthlight'>Next JS</span>
-								<span className='higthlight'>Node JS</span>
-								<span className='higthlight'>Mysql</span>
-								<span className='higthlight'>Flutter</span>
-							</TextLoop>
-						</div>
-					</div>
-					<div className="mt-16">
-						<a className="btn_action bg_content" href="/some-url">
-							<BsInstagram />
-							<span>Instagram</span>
-						</a>
-					</div>
-					<div className="mt-16">
-						<a className="btn_action bg_content" href="/some-url">
-							<FaTiktok />
-							<span>Tiktok</span>
-						</a>
-					</div>
-					<div className="mt-16">
-						<a className="btn_action bg_content" href="/some-url">
-							<FiYoutube />
-							<span>Youtube</span>
-						</a>
-					</div>
-
-				</div>
-			</div>
-			<ParticlesBg type="thick" bg={true} />
-		</div>
-	);
+            <div className="bg_content rd_12 p_8 mt-16">
+              Familiar with{" "}
+              <TextLoop interval={800}>
+                <span className="higthlight">Adobe Photoshop</span>
+                <span className="higthlight">Adobe Ilustrator</span>
+                <span className="higthlight">Adobe After Effects</span>
+                <span className="higthlight">Adobe Premier Pro</span>
+                <span className="higthlight">Adobe Lightroom</span>
+                {/* <span className="higthlight">Mysql</span>
+                <span className="higthlight">Flutter</span> */}
+              </TextLoop>
+            </div>
+          </div>
+          <div className="mt-16">
+            <a
+              className="btn_action bg_content"
+              href="https://www.instagram.com/riost._/"
+            >
+              <BsInstagram />
+              <span>riost._</span>
+            </a>
+          </div>
+          <div className="mt-16">
+            <a
+              className="btn_action bg_content"
+              href="https://www.instagram.com/m_riosetyo/"
+            >
+              <BsInstagram />
+              <span>m_riosetyo</span>
+            </a>
+          </div>
+          <div className="mt-16">
+            <a
+              className="btn_action bg_content"
+              href="https://www.instagram.com/jai_gift/"
+            >
+              <BsInstagram />
+              <span>jai_gift</span>
+            </a>
+          </div>
+          <div className="mt-16">
+            <a className="btn_action bg_content" href="/some-url">
+              <GiPapers />
+              <span>Portfolio</span>
+            </a>
+          </div>
+          <div className="mt-16">
+            <a
+              className="btn_action bg_content"
+              href="https://wa.me/6285776288381"
+            >
+              <MdOutlineContactPhone />
+              <span>Contact Person</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <ParticlesBg type="thick" bg={true} />
+    </div>
+  );
 }
 
 export default App;
